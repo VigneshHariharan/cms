d7ff07bc79131c6ec3d9e517989b47f57fb40509
sathiya01 <shreejabalakrishnan0131@gmail.com>
2019-08-27 15:59:58 +0530

    Attached form inside dashboard page

--- src/Components/Dashboard/userDashboard.js
+++ src/Components/Dashboard/userDashboard.js
@@ -2,8 +2,19 @@ import React, { Component } from 'react'
 
 import Table from "./Table"
 import ComplaintForm from "./complaintForm"
+import Form from './technicianForm/form'
 
 export default class UserDashboard extends Component {
+  constructor(){
+    super()
+    this.state={
+      visible:false,
+    }
+  }
+  handleClick=()=>{
+    this.setState({visible:!this.state.visible})
+  }
+
   render() {
     return (
       <div>
@@ -21,6 +32,9 @@ export default class UserDashboard extends Component {
         {
           this.props.show ? <ComplaintForm show={this.props.handleClick} /> : ""
         }
+        <br/><br/>
+        <button onClick={this.handleClick}>Add a Technician</button>
+        {this.state.visible?<Form/>:''}
 
       </div>
     )
