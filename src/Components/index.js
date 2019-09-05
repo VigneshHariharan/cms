3d94ee84886130b259ad2297836294fd191adaed
Vignesh <vigneshph7@gmail.com>
2019-09-05 14:11:46 +0530

    minor changes

--- src/Components/index.js
+++ src/Components/index.js
@@ -2,13 +2,11 @@ import React, { Component } from 'react'
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom"
 import { Provider } from "react-redux"
 import store from "./reduxManager"
-// import ProtectRoutes from './protectRoutes';
 
 import LoginPage from "./Login/loginpage"
 import Dashboard from "./Dashboard/Dashboard"
 import AdminDashboard from "./Dashboard/adminDashboard"
 import TechnicianDashboard from "./Dashboard/technicianDashboard"
-import ComplaintForm from "./Dashboard/complaintForm"
 
 
 
@@ -23,13 +21,11 @@ export default class Home extends Component {
               <Route exact path="/dashboard/" component={Dashboard} />
               <Route exact path="/admindashboard/" component={AdminDashboard} />
               <Route exact path="/techniciandashboard/" component={TechnicianDashboard} />
-              <Route exact path="/dashboard/complaintform/" component={ComplaintForm} />
-              <Route exact path="/admindashboard/complaintform/" component={ComplaintForm} />
               <Route exact path="*" component={() => <h1>Different Page</h1>} />
             </Switch>
           </Provider>
         </Router>
-      </div >
+      </div>
     )
   }
 }
